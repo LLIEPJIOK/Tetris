b7e87aab037298b23600f1cfb4432bf11cbe091c
Matvey Kvetko <88326289+Emoless@users.noreply.github.com>
2024-01-02 19:39:39 +0300

    Interface 0.1

--- src/PlayArea.java
+++ src/PlayArea.java
@@ -1,23 +1,77 @@
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Objects;
 
 public class PlayArea extends JFrame {
-    private final Field field;
+    private Field field;
+    private Panel backgroundPanel;
 
-    {
+    private JButton pauseButton;
+    private JButton exitButton;
+
+
+    PlayArea() {
+        SetupMainPanel();
+        SetupPlayPanel();
+        SetupBackgroundPanel();
+        this.add(field);
+        this.add(backgroundPanel);
+    }
+
+    private void SetupMainPanel() {
         setTitle("Tetris");
-        setSize(600, 450);
+        setIconImage(new ImageIcon(Objects.requireNonNull(PlayArea.class.getResource("tetris.png"))).getImage());
+        setSize(335, 480);
         setLocationRelativeTo(null);
         setResizable(false);
-
-        field = new Field();
-        field.setLocation(0, 0);
-
-        this.add(field);
-        this.add(new Panel());
     }
+
+    private void SetupPlayPanel() {
+        field = new Field();
+        field.setLocation(20, 20);
+    }
+
+    private void SetupBackgroundPanel()
+    {
+        backgroundPanel = new Panel();
+        backgroundPanel.setBackground(new Color(253, 208, 59));
+        backgroundPanel.setLayout(null);
+        SetupButtonPause();
+        SetupButtonExit();
+        backgroundPanel.add(pauseButton);
+        backgroundPanel.add(exitButton);
+    }
+
+    private JButton SetupButton(String name, Color color)
+    {
+        JButton button = new JButton(name);
+        button.setBackground(color);
+        button.setForeground(Color.WHITE);
+        Border border = new LineBorder(new Color(0, 0, 0), 2, false);
+        button.setBorder(border);
+        Font font = new Font("Arial", Font.BOLD, 16);
+        button.setFont(font);
+        button.setForeground(new Color(0, 0, 0, 255));
+        button.setFocusPainted(false);
+        return button;
+    }
+
+    private void SetupButtonPause()
+    {
+        pauseButton = SetupButton("Pause", new Color(128, 215, 84));
+        pauseButton.setBounds(235,345,70,30);
+    }
+
+    private void SetupButtonExit()
+    {
+        exitButton = SetupButton("Exit", new Color(253, 58, 58, 255));
+        exitButton.setBounds(235,390,70,30);
+    }
+
 
     @Override
     public void paint(Graphics g) {
